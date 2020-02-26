65f582def874b0397b55a4f032d00b2f8cdb4edc
9mike1 <39293095+9mike1@users.noreply.github.com>
2020-02-26 19:54:20 +0000

    Version26022020

---
+++ Assets/Scripts/sawMill.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sawMill : MonoBehaviour
+{
+    private int amount;
+    public int timeNeedForProcess;
+    private float lastprocessed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        proceed("woodPlanks");
+    }
+
+    public void proceed(string produces)
+    {
+        lastprocessed += Time.deltaTime;
+
+        if (getResourceAmount("wood") >= 2 && lastprocessed >= timeNeedForProcess)
+        {
+
+            changeResource("wood", -2);
+            changeResource(produces, 1);
+            lastprocessed = 0;
+
+        }
+    }
+
+    public void changeResource(string resource, int amount)
+    {
+        
+        GameObject.Find(resource+"Amount").GetComponent<UnityEngine.UI.Text>().text = (int.Parse(GameObject.Find(resource + "Amount").GetComponent<UnityEngine.UI.Text>().text) + amount).ToString();
+    }
+
+    
+    
+    int getResourceAmount(string resource)
+    {
+        amount = int.Parse(GameObject.Find(resource+"Amount").GetComponent<UnityEngine.UI.Text>().text);
+        return amount;
+    }
+}
---
+++ Assets/Scripts/woodcuter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class woodcuter : MonoBehaviour
+{
+
+
+    public float timeinSeconds;
+    public int amountWhileThisTime;
+    public string itemneeds;
+    public int itemNeedCount;
+
+    private void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
