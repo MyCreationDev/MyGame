91ba8c5f59676a87f3930ed3467435e9f8e0e879
Bedrettin-Entw <61382245+Bedrettin-Entw@users.noreply.github.com>
2020-02-27 18:46:59 +0100

    Basic Controls eingefügt

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public GameObject Enviroment;
+    public GameObject BuildMenu;
+    private GameObject CurrentlySelectedPrefab;
+    private bool isBuilding = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            isBuilding = false;
+        }
+        if(isBuilding == true)
+        {
+            var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitinfo;
+            Physics.Raycast(Ray, out hitinfo);
+            CurrentlySelectedPrefab.transform.position = hitinfo.point;
+        }
+    }
+
+    public void Build(GameObject PrefabToBuild)
+    {
+        CurrentlySelectedPrefab = Instantiate(PrefabToBuild, Enviroment.transform);
+        isBuilding = true;
+    }
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public int CameraSpeed;
+    public bool StopMovement;
+
+    public int CameraMoveArea;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+            StopMovement = !StopMovement;
+
+        var Pos = transform.position;
+        if(Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - CameraMoveArea)
+        {
+            Pos.z += CameraSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.S) || Input.mousePosition.y <= CameraMoveArea)
+        {
+            Pos.z -= CameraSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - CameraMoveArea)
+        {
+            Pos.x += CameraSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.A) || Input.mousePosition.x <=  CameraMoveArea)
+        {
+            Pos.x -= CameraSpeed * Time.deltaTime;
+        }
+        if(!StopMovement)
+            transform.position = Pos;
+
+        
+    }
+}
