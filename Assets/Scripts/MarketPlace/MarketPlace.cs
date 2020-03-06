e918fd53bb9da061d9fafbe6d9331411442426c0
9mike1 <39293095+9mike1@users.noreply.github.com>
2020-03-06 20:11:29 +0100

    MarketPLace

---
+++ Assets/Scripts/MarketPlace/MarketPlace.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Linq;
+using UnityEngine.UI;
+
+public class MarketPlace : MonoBehaviour
+{
+    public Slider mainSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getRessourceInformation("wood", "normalPrice");
+    }
+
+    public void getRessourceInformation(string resource, string information)
+    {
+        TextAsset textXMLAsset = Resources.Load<TextAsset>("resourceList");
+        var doc = XDocument.Parse(textXMLAsset.text);
+        var alldict = doc.Element("resources").Elements();
+        foreach (var oneDict in alldict)
+        {
+            if (oneDict.Name == resource)
+            {
+                foreach (var singleDict in oneDict.Elements(information))
+                {
+                    //Abgefragten Wert zurückgeben
+                    //Debug.Log(singleDict.Value);
+                }
+            }
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/MarketPlace/Slider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slider : MonoBehaviour
+{
+    public GameObject Amount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sliderValues();
+    }
+    public void sliderValue(float value)
+    {
+        //Debug.Log(value);
+
+        //Debug.Log(Amount.GetComponent<Text>().text);
+        gameObject.GetComponentInChildren<Text>().text = (Mathf.Round(GameManager.Instance.wood * value)).ToString();
+        //Debug.Log(transform.Find("/Managers/UI/completeUI/ressources/wood/woodAmount").GetComponent<TextMesh>().text); //").GetComponent<Text>().text);
+    }
+}
